8eddf155512b096b57d5fd1710713cf099e4e16f
rajitkumar <krajit909@gmail.com>
2022-12-30 23:07:28 +0530

    HomeJs created

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
+    <>
     <div>Home</div>
+    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </>
   )
 }
 
