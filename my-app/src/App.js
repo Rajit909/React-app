61353cc9b00fe0deb5d935e69162340a44690744
rajitkumar <krajit909@gmail.com>
2022-12-30 23:31:04 +0530

    props used

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 function App() {
   return (
     <>
-    <Navbar/>
+    <Navbar title="Home"/>
     <Home/>
     </>
   );
--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar(props) {
   return (
     <>
-    <nav className="navbar bg-light">
+    <nav className="navbar bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Home
+    <a className="navbar-brand text-white" href=" ">
+      {props.title}
     </a>
   </div>
 </nav>
